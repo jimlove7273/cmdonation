3cfe3d51618b4e0dd899c1a6a334c27484ef6cf1
MorganLewis01 <149626654+MorganLewis01@users.noreply.github.com>
2026-01-03 22:27:46 -0800

    Display Check # as PayPal or Zelle

--- components/donations-view.tsx
+++ components/donations-view.tsx
@@ -477,7 +477,11 @@ export function DonationsView() {
                       {donation.Type || ''}
                     </td>
                     <td className="px-6 py-4 text-sm text-gray-900">
-                      {donation.Check || ''}
+                      {donation.Check.toLowerCase() === 'paypal'
+                        ? 'PayPal'
+                        : donation.Check.toLowerCase() === 'zelle'
+                        ? 'Zelle'
+                        : donation.Check}
                     </td>
                     <td className="px-6 py-4 text-sm text-gray-900">
                       ${(donation.Amount || 0).toFixed(2)}
